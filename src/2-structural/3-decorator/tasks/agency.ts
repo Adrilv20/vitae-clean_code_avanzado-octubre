9e7543a93427579d386815f16670f9c9dff44d9e
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-24 19:00:07 +0200

    feat: decorator tasks

--- src/2-structural/3-decorator/tasks/agency.ts
+++ src/2-structural/3-decorator/tasks/agency.ts
@@ -7,3 +7,18 @@ export class Agency {
     return new Booking(bookingId, trip, user, price, "Pending", new Date(), new Date());
   }
 }
+
+// export class AgencyEmailDecorator implements BookingCreator {
+//   private agency = new Agency();
+//   public createBooking(trip: string, price: number): Booking {
+//     return this.agency.createBooking(trip, price);
+//   }
+//   public createBookingEmail(trip: string, price: number, email?: string): Booking {
+//     const bookingId = Math.floor(Math.random() * 100);
+//     const user = "";
+//     if (email) {
+//       console.log("📧 sending email to ", email);
+//     }
+//     return new Booking(bookingId, trip, user, price, "Pending", new Date(), new Date());
+//   }
+// }
