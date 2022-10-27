20708cfe31583d76fcc41d8e8cb3413c4535cdca
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-27 09:51:12 +0200

    feat: facade tasks

--- src/2-structural/4-facade/tasks/3-facade_bookings.ts
+++ src/2-structural/4-facade/tasks/3-facade_bookings.ts
@@ -1,4 +1,42 @@
 // ! npm run 2-4-3
-// import { Bookings } from "./bookings";
-// import { Notifier } from "./notifier";
-// import { Payments } from "./payments";
+import { Bookings } from "./bookings";
+import { Notifier } from "./notifier";
+import { Payments } from "./payments";
+export class AgencyFacade {
+  // * 🤩 one class to rule them all
+  private bookings: Bookings = new Bookings();
+  private payments: Payments = new Payments();
+  private notifier: Notifier = new Notifier();
+
+  public createBooking(trip: string): string {
+    // * 🤩 encapsulates internal know how
+    const tripPrice = this.bookings.getPrice(trip);
+    const paymentsResult = this.payments.makePayment(trip, tripPrice);
+    const bookingsResult = this.bookings.makeBooking(trip, paymentsResult);
+    const notifierResult = this.notifier.notify(trip, "booked");
+    return `💸 ${paymentsResult} +  📅 ${bookingsResult} + 📧 ${notifierResult}`;
+  }
+  public cancelBooking(trip: string): string {
+    const tripPrice = this.bookings.getPrice(trip);
+    const paymentsResult = this.payments.makeRefund(trip, tripPrice);
+    const bookingsResult = this.bookings.cancelBooking(trip, paymentsResult);
+    const notifierResult = this.notifier.notify(trip, "canceled");
+    return `💸 ${paymentsResult} +  📅 ${bookingsResult} + 📧 ${notifierResult}`;
+  }
+}
+
+export class Client {
+  // * 🤩 only one dependency
+  private agency: AgencyFacade = new AgencyFacade();
+
+  // * 🤩 maximum abstraction level, not always possible
+  public createBooking(trip: string): string {
+    return this.agency.createBooking(trip);
+  }
+  public cancelBooking(trip: string): string {
+    return this.agency.cancelBooking(trip);
+  }
+}
+const client = new Client();
+console.log(client.createBooking("Paris"));
+console.log(client.cancelBooking("Paris"));
